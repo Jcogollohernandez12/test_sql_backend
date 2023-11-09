d22df78ca380add0711464cdd3e8073fc0d44854
JcogolloHernandez12 <josandres123@gmail.com>
2023-11-08 22:38:07 -0500

    port

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.use(cors());
 app.use(express.json()); 
 const port = 3000;
 app.use('/', routes);
-app.listen(port, () => {
+app.listen(49241, () => {
     console.log(`Servidor en ejecución en el puerto ${port}`);
 });
